ec29039fecd795e58ba641e20d401859fa68139d
skjapps <srikanthjakka@hotmail.co.uk>
2023-05-06 15:11:28 +0100

    Initial website commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var h1 = document.querySelector('h1');
+	var h2 = document.querySelector('h2');
+	
+	h1.classList.add('fade-in');
+	h2.classList.add('fly-up');
+});
