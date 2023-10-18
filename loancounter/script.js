6f5d55d326d677f542abc2441a4a297f77e3cbd8
skjapps <srikanthjakka@hotmail.co.uk>
2023-10-18 10:31:27 +0100

    Improved drum roll sound effect, added crickets

--- loancounter/script.js
+++ loancounter/script.js
@@ -7,7 +7,10 @@ const counterPoundsElement = document.getElementById('counter-pounds');
 const counterDecimalElement = document.getElementById('counter-decimal');
 
 // Drum Roll sound effect
-var drumRoll = new Audio('loancounter/assets/snd/drumroll.mp3');
+var drumRoll = new Audio('loancounter/assets/snd/drumroll2.mp3');
+
+// Crickets background sound effect
+var crickets = new Audio('loancounter/assets/snd/crickets.mp3');
 
 // The title changes now
 let debtShown = false;
