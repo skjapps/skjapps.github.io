adc7f3bc09c671b2e070e577a09a01ef17858b65
skjapps <srikanthjakka@hotmail.co.uk>
2023-10-20 16:12:09 +0100

    new index work

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
 	h1.classList.add('fade-in');
 	h2.classList.add('fly-up');
 });
+
+const background = document.querySelector(".background");
+
+background.addEventListener("mousemove", e => {
+  const mouseX = e.clientX / window.innerWidth - 0.5;
+  const mouseY = e.clientY / window.innerHeight - 0.5;
+
+  background.style.setProperty("--mouseX", mouseX);
+  background.style.setProperty("--mouseY", mouseY);
+});
+
